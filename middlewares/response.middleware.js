6917111c5a95fd2f5d01ad2e84a0940e2a752eb4
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-05-16 13:41:05 +0300

    implemented respose.middleware

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,6 @@
 const responseMiddleware = (req, res, next) => {
    // TODO: Implement middleware that returns result of the query
+    res.err ? res.send(res.err) : res.send(res.data);
     next();
 }
 
