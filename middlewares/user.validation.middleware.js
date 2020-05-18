bd8ef54c253c06f3a87f74b0f49ab2b46824572e
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-05-18 14:21:01 +0300

    decompose validation middleware

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -4,30 +4,16 @@ const BAD_REQUEST_STATUS = 400;
 
 const createUserValid = (req, res, next) => {
     // TODO: Implement validatior for user entity during creation
+    validations.forEach((message, validation) => {
+        if (!validation(req.body)) {
+            valid = false;
+            res.status = BAD_REQUEST_STATUS;
+            res.send({ error: true, message });
+        }
+    });
 
-    //check for existing required properties 
-    if (!hasRequiredFields(req.body)) {
-        res.status = BAD_REQUEST_STATUS;
-        throw new Error({ error: true,  message: 'Not enough data to create user'});
-    }
-
-    //check for validity of fields
-    if (!emailIsValid(req.body.email)) {
-        res.status = BAD_REQUEST_STATUS;
-        throw new Error({ error: true,  message: 'Not valid email. Use only gmail.com'});
-    }
-
-    if (!phoneNumberIsValid(req.body.phoneNumber)) {
-        res.status = BAD_REQUEST_STATUS;
-        throw new Error({ error: true,  message: 'Not valid phone number. It should be in \'+380xxxxxxxxx\' format'});
-    }
-    
-    if (!passwordIsvalid(req.body.password)) {
-        res.status = BAD_REQUEST_STATUS;
-        throw new Error({ error: true, message: 'Not valid password. It should contain at least 3 characters and not contain \'password\'.'});
-    }
-    
     next();
+
 }
 
 const updateUserValid = (req, res, next) => {
@@ -36,23 +22,25 @@ const updateUserValid = (req, res, next) => {
     next();
 }
 
-const passwordIsValid = password => {
+const passwordIsValid = ({ password }) => {
     const incorrectLength = password.length < 3;
     const passwordIncluded = password.toLowerCase().includes('password');     
     if (incorrectLength || passwordIncluded ) {
+        
         return false;
     }
     return true;
-};
 
-const emailIsValid = email => {
+};  
+
+const emailIsValid = ({ email }) => {
     const re = /^[\w.+\-]+@gmail\.com$/;
     return re.test(email);
 }
 
-const phoneNumberIsValid = number => {
+const phoneNumberIsValid = ({ phoneNumber }) => {
     const re = /^\+?3?8?(0\d{9})$/;
-    return re.test(number);
+    return re.test(phoneNumber);
 }
 
 const hasRequiredFields = body => {
@@ -66,6 +54,15 @@ const hasRequiredFields = body => {
     }
     return hasRequiredFields;
 }
+
+
+const validations = new Map([
+    [hasRequiredFields, 'Not enough data to create user'],
+    [emailIsValid, 'Not valid email. Use only gmail.com'],
+    [phoneNumberIsValid, 'Not valid phone number. It should be in \'+380xxxxxxxxx\' format'],
+    [passwordIsValid, 'Not valid password. It should contain at least 3 characters and not contain \'password\'.']
+]);
+
 
 exports.createUserValid = createUserValid;
 exports.updateUserValid = updateUserValid;
