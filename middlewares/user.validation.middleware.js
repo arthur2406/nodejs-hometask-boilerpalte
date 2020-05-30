0949f88246349481557c1c88f7bdc686db22b5c5
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-05-30 23:39:53 +0300

    Refactor validation middleware

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -2,67 +2,61 @@ const { user } = require('../models/user');
 
 const BAD_REQUEST_STATUS = 400;
 
-const createUserValid = (req, res, next) => {
-    // TODO: Implement validatior for user entity during creation
-    validations.forEach((message, validation) => {
-        if (!validation(req.body)) {
-            valid = false;
-            res.status = BAD_REQUEST_STATUS;
-            res.send({ error: true, message });
-        }
-    });
+const userValid = (req, res, next) => {
+  let errorNotOccured = true;
+  validations.forEach((message, validation) => {
+    if (!validation(req.body) && errorNotOccured) {
+      errorNotOccured = false;
+      res.status = BAD_REQUEST_STATUS;
+      res.send({ error: true, message });
+    }
+  });
 
-    next();
+  if (errorNotOccured) next();
 
 }
 
-const updateUserValid = (req, res, next) => {
-    // TODO: Implement validatior for user entity during update
-
-    next();
-}
 
 const passwordIsValid = ({ password }) => {
-    const incorrectLength = password.length < 3;
-    const passwordIncluded = password.toLowerCase().includes('password');     
-    if (incorrectLength || passwordIncluded ) {
-        
-        return false;
-    }
-    return true;
+  const incorrectLength = password.length < 3;
+  const passwordIncluded = password.toLowerCase().includes('password');
+  if (incorrectLength || passwordIncluded) {
 
-};  
+    return false;
+  }
+  return true;
+
+};
 
 const emailIsValid = ({ email }) => {
-    const re = /^[\w.+\-]+@gmail\.com$/;
-    return re.test(email);
+  const re = /^[\w.+\-]+@gmail\.com$/;
+  return re.test(email);
 }
 
 const phoneNumberIsValid = ({ phoneNumber }) => {
-    const re = /^\+?3?8?(0\d{9})$/;
-    return re.test(phoneNumber);
+  const re = /^\+?3?8?(0\d{9})$/;
+  return re.test(phoneNumber);
 }
 
 const hasRequiredFields = body => {
-    let hasRequiredFields = true;
-    for (const prop in user) {
-        if (body.hasOwnProperty(prop) && prop !== 'id') {
-            hasRequiredFields = true;
-        } else {
-            hasRequiredFields = false;
-        }
+  let hasRequiredFields = true;
+  for (const prop in user) {
+    if (body.hasOwnProperty(prop) && prop !== 'id') {
+      hasRequiredFields = true;
+    } else {
+      hasRequiredFields = false;
     }
-    return hasRequiredFields;
+  }
+  return hasRequiredFields;
 }
 
 
 const validations = new Map([
-    [hasRequiredFields, 'Not enough data to create user'],
-    [emailIsValid, 'Not valid email. Use only gmail.com'],
-    [phoneNumberIsValid, 'Not valid phone number. It should be in \'+380xxxxxxxxx\' format'],
-    [passwordIsValid, 'Not valid password. It should contain at least 3 characters and not contain \'password\'.']
+  [hasRequiredFields, 'Not enough data to create user'],
+  [emailIsValid, 'Not valid email. Use only gmail.com'],
+  [phoneNumberIsValid, 'Not valid phone number. It should be in \'+380xxxxxxxxx\' format'],
+  [passwordIsValid, 'Not valid password. It should contain at least 3 characters and not contain \'password\'.']
 ]);
 
 
-exports.createUserValid = createUserValid;
-exports.updateUserValid = updateUserValid;
+exports.userValid = userValid;
