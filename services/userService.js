bb3b6d010ce8e65f4968f8b7241c732f92cedd0f
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-05-30 23:40:25 +0300

    Added crud funcs to userService

--- services/userService.js
+++ services/userService.js
@@ -2,15 +2,31 @@ const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
 
-    // TODO: Implement methods to work with user
+  // TODO: Implement methods to work with user
+  delete(id) {
+    const user = UserRepository.delete(id);
+    return user;
+  }
 
-    search(search) {
-        const item = UserRepository.getOne(search);
-        if(!item) {
-            return null;
-        }
-        return item;
-    }
+  update(id, dataToUpdate) {
+    const user = UserRepository.update(id, dataToUpdate);
+    return user;
+  }
+
+  create(data) {
+    const user = UserRepository.create(data);
+    return user;
+  }
+
+  searchAll() {
+    const items = UserRepository.getAll()
+    return items;
+  }
+
+  search(search) {
+    const item = UserRepository.getOne(search);
+    return item;
+  }
 }
 
 module.exports = new UserService();
