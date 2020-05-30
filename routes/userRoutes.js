b426aac4b83a2e955d2332186b50df1e361bbb33
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-05-30 23:40:46 +0300

    Added routes to user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,60 @@
 const { Router } = require('express');
 const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
+const { userValid } = require('../middlewares/user.validation.middleware');
 const { responseMiddleware } = require('../middlewares/response.middleware');
 
 const router = Router();
+
+
+router.get('/', (req, res, next) => {
+  const users = UserService.searchAll();
+  res.data = users;
+  res.status = 200;
+  next();
+}, responseMiddleware);
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const user = UserService.search({ id });
+  if (user) {
+    res.data = user;
+    res.status = 200;
+  } else {
+    res.err = { error: true, message: 'User not found' };
+    res.status = 404;
+  }
+  next();
+}, responseMiddleware);
+
+
+router.post('/', userValid, (req, res, next) => {
+  const user = UserService.create(req.body);
+  res.data = user;
+  res.status = 201;
+  next();
+}, responseMiddleware);
+
+
+router.put('/:id', userValid, (req, res, next) => {
+  const user = UserService.update(req.params.id, req.body);
+  res.data = user;
+  res.status = 204;
+  next();
+}, responseMiddleware);
+
+router.delete('/:id', (req, res, next) => {
+  const user = UserService.delete(req.params.id);
+  if (user) {
+    res.data = user;
+    res.status = 204;
+  } else {
+    res.err = { error: true, message: 'User not found' };
+    res.status = 404;
+  }
+  next();
+
+}, responseMiddleware);
+
 
 // TODO: Implement route controllers for user
 
