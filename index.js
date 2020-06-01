a9feed08cf73d1ff39627d9f6ab485efcf745b11
Arthur Ryzhyy <rizhiy2406@gmail.com>
2020-06-01 21:21:41 +0300

    minor changes

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 const routes = require('./routes/index');
 routes(app);
 
+
 app.use('/', express.static('./client/build'));
 
 const port = 3050;
-app.listen(port, () => {});
+app.listen(port, () => {console.log('Server is running')});
 
 exports.app = app;
